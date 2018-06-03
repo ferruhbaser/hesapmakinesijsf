9eeb04e244a97eacb1aba1c30097c6788090f650
FRH <FRH@192.168.1.45>
2018-06-03 16:00:52 +0300

    index.xhtml oluşturduk.

---
+++ src/main/java/com/bilisimegitim/hesapmakinesijsf/mb/HesapMakinesi.java
@@ -0,0 +1,46 @@
+
+package com.bilisimegitim.hesapmakinesijsf.mb;
+
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+
+
+@Named(value = "hesapMakinesi")
+@RequestScoped
+public class HesapMakinesi {
+
+    
+    public HesapMakinesi() {
+    }
+    
+    private double sayi1;
+    private double sayi2;
+    private double sonuc;
+
+    public double getSayi1() {
+        return sayi1;
+    }
+
+    public void setSayi1(double sayi1) {
+        this.sayi1 = sayi1;
+    }
+
+    public double getSayi2() {
+        return sayi2;
+    }
+
+    public void setSayi2(double sayi2) {
+        this.sayi2 = sayi2;
+    }
+
+    public double getSonuc() {
+        return sonuc;
+    }
+
+    public void setSonuc(double sonuc) {
+        this.sonuc = sonuc;
+    }
+    
+    
+    
+}
